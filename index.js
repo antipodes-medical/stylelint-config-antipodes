7fef2ac48254c3e7e73dc42c8faf51dc0fe3e5d2
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2022-05-17 20:34:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -184,6 +184,88 @@ module.exports = {
       "user-select",
       "animation",
       "transition"
-    ]
+    ],
+        "scss/selector-no-redundant-nesting-selector": true,
+    "scss/at-else-closing-brace-newline-after": "always-last-in-chain",
+    "scss/at-else-closing-brace-space-after": "always-intermediate",
+    "scss/at-else-empty-line-before": "never",
+    "scss/at-else-if-parentheses-space-before": "always",
+    "scss/at-function-parentheses-space-before": "never",
+    "scss/at-function-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    "scss/at-mixin-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    "scss/percent-placeholder-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    "scss/at-if-closing-brace-newline-after": "always-last-in-chain",
+    "scss/at-if-closing-brace-space-after": "always-intermediate",
+    "scss/at-import-no-partial-leading-underscore": true,
+    "scss/at-import-partial-extension": "never",
+    "scss/at-mixin-argumentless-call-parentheses": "always",
+    "scss/at-mixin-parentheses-space-before": "never",
+    "scss/at-use-no-unnamespaced": true,
+    "scss/dollar-variable-colon-newline-after": "always-multi-line",
+    "scss/dollar-variable-colon-space-after": "always-single-line",
+    "scss/dollar-variable-colon-space-before": "never",
+    "scss/dollar-variable-empty-line-after": [
+      "always",
+      {
+        "except": [
+          "last-nested",
+          "before-dollar-variable"
+        ],
+        "ignore": [
+          "inside-single-line-block"
+        ]
+      }
+    ],
+    "scss/dollar-variable-empty-line-before": [
+      "always",
+      {
+        "except": [
+          "first-nested",
+          "after-dollar-variable"
+        ],
+        "ignore": [
+          "after-comment",
+          "inside-single-line-block"
+        ]
+      }
+    ],
+    "scss/dollar-variable-first-in-block": [
+      true,
+      {
+        "ignore": [
+          "comments",
+          "imports"
+        ]
+      }
+    ],
+    "scss/dollar-variable-no-missing-interpolation": true,
+    "scss/double-slash-comment-empty-line-before": [
+      "always",
+      {
+        "except": [
+          "first-nested"
+        ],
+        "ignore": [
+          "between-comments",
+          "inside-block",
+          "stylelint-commands"
+        ]
+      }
+    ],
+    "scss/double-slash-comment-whitespace-inside": "always",
+    "scss/dimension-no-non-numeric-values": true,
+    "scss/operator-no-newline-after": true,
+    "scss/operator-no-newline-before": true,
+    "scss/operator-no-unspaced": true,
+    "scss/no-duplicate-dollar-variables": [
+      true,
+      {
+        "ignoreInside": [
+          "at-rule",
+          "nested-at-rule"
+        ]
+      }
+    ],
+    "scss/no-duplicate-mixins": true
   }
 }
